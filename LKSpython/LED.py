f5fd0a19d45f4d66ed17143550e0e78563509ba6
IndoorGrowLab <53471446+IndoorGrowLab@users.noreply.github.com>
2019-08-07 01:21:02 +0200

    Add files via upload
    Aktualisierung der Kommentare und der einzelnen Funktionen

--- LKSpython/LED.py
+++ LKSpython/LED.py
@@ -1,15 +1,32 @@
 # Die Klasse LED erstellt.
 class LED:
 
-    # Ein Objekt der Klasse wird mit den Parametern Name, Farbe, Frequenz und Helligkeit intialisiert.
+
     def __init__(self, name, farbe, frequenz, helligkeit):
+        """
+
+        :param name: Der Name der LED z.b. 1 oder eins
+        :param farbe: Die Farbe der LED
+        :param frequenz: Die Frequenz der LED
+        :param helligkeit: Die Helligkeit der LED
+        """
+
         self.name = name
         self.farbe = farbe
         self.frequenz = frequenz
         self.helligkeit = helligkeit
 
-    def get_parameter(self, objekt):
+    def get_LED(self, objekt):
+        """
+
+        :param objekt: Eine LED
+        :return: die aktuellen Parameter der LED
+        """
+        #Die aktuellen Werte einer LED werden in Form eine dictionarys zurück gegeben.
         return {objekt.name: "Name", objekt.farbe : "Farbe", objekt.frequenz : "Frequenz", objekt.helligkeit : "Helligkeit"}
+
+#    def update_LED(self):
+
 
 
 
--- LKSpython/LKS.py
+++ LKSpython/LKS.py
@@ -1,12 +1,30 @@
 import LED
 def init_LED(Namen, Farben, Frequenzen, Helligkeiten, value):
+    """
+
+    :param Namen: Eine Liste mit Namen von einer Menge von LEDs
+    :param Farben: Eine Liste mit Farben von einer Menge von LEDs
+    :param Frequenzen: Eine Liste mit Frequenzen von einer Menge von LEDs
+    :param Helligkeiten: Eine Liste mit Helligkeit von einer Menge von LEDs
+    :param value: Ein Integer-Wert für die Anazhl an LEDs
+    :return: Eine Liste mit Objekten der Klasse LED, wobei die Eigenschaften des i-ten Objekte denen des i-ten Listenelments entsprechen
+    """
     LEDs = [LED.LED(Namen[i], Farben[i], Frequenzen[i], Helligkeiten[i]) for i in range(value)]
     print(LEDs)
     return LEDs
-Namen = ["eins", "zwei", "drei"]
-Farben = ["rot", "blau", "UV"]
-Frequenzen = [700, 1200, 1600]
-Helligkeit = [1000, 2000, 4000]
-A = init_LED(Namen, Farben, Frequenzen, Helligkeit, 3)
-for i in A:
-    print(i.get_parameter(i))
+
+
+########################################################################################################################
+                                                    TEST
+########################################################################################################################
+# def tagsim(LEDs):
+#     for LED in LEDs:
+#         if LED.farbe == "rot":
+#
+# Namen = ["eins", "zwei", "drei"]
+# Farben = ["rot", "blau", "UV"]
+# Frequenzen = [700, 1200, 1600]
+# Helligkeit = [1000, 2000, 4000]
+# A = init_LED(Namen, Farben, Frequenzen, Helligkeit, 3)
+# for i in A:
+#     print(i.get_LED(i))
