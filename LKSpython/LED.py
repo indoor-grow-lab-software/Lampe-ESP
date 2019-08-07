ea70e956729e69f9d7d882998ee89f7e18e559a0
IndoorGrowLab <53471446+IndoorGrowLab@users.noreply.github.com>
2019-08-07 14:25:11 +0200

    Add files via upload
    Updating der tagsim funktion sowie überarbeitung der Klasse LED

--- LKSpython/LED.py
+++ LKSpython/LED.py
@@ -2,30 +2,28 @@
 class LED:
 
 
-    def __init__(self, name, farbe, frequenz, helligkeit):
+    def __init__(self, name, farbe, helligkeit):
         """
 
         :param name: Der Name der LED z.b. 1 oder eins
         :param farbe: Die Farbe der LED
-        :param frequenz: Die Frequenz der LED
         :param helligkeit: Die Helligkeit der LED
         """
 
         self.name = name
         self.farbe = farbe
-        self.frequenz = frequenz
         self.helligkeit = helligkeit
 
-    def get_LED(self, objekt):
+    def get(self, objekt):
         """
 
         :param objekt: Eine LED
         :return: die aktuellen Parameter der LED
         """
         #Die aktuellen Werte einer LED werden in Form eine dictionarys zurück gegeben.
-        return {objekt.name: "Name", objekt.farbe : "Farbe", objekt.frequenz : "Frequenz", objekt.helligkeit : "Helligkeit"}
+        return {objekt.name: "Name", objekt.farbe : "Farbe", objekt.helligkeit : "Helligkeit"}
 
-#    def update_LED(self):
+#    def update(self):
 
 
 
--- LKSpython/LKS.py
+++ LKSpython/LKS.py
@@ -1,30 +1,92 @@
 import LED
-def init_LED(Namen, Farben, Frequenzen, Helligkeiten, value):
+
+Farben = ['rot', 'blau', 'blau', 'rot', 'weiß', 'ir', 'blau', 'rot', 'blau', 'uv', 'weiß', 'gruen', 'rot', 'weiß', 'blau', 'rot']
+Namen = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+Helligkeit = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+def init_LED(Namen, Farben, Helligkeiten, value):
     """
 
     :param Namen: Eine Liste mit Namen von einer Menge von LEDs
     :param Farben: Eine Liste mit Farben von einer Menge von LEDs
-    :param Frequenzen: Eine Liste mit Frequenzen von einer Menge von LEDs
     :param Helligkeiten: Eine Liste mit Helligkeit von einer Menge von LEDs
     :param value: Ein Integer-Wert für die Anazhl an LEDs
     :return: Eine Liste mit Objekten der Klasse LED, wobei die Eigenschaften des i-ten Objekte denen des i-ten Listenelments entsprechen
     """
-    LEDs = [LED.LED(Namen[i], Farben[i], Frequenzen[i], Helligkeiten[i]) for i in range(value)]
+    LEDs = [LED.LED(Namen[i], Farben[i], Helligkeiten[i]) for i in range(value)]
     print(LEDs)
     return LEDs
 
 
+
+def tagsim(LEDs,fade):
+    """
+
+    :param fade: Die steigung der aktualisierungs Funktion, je kleiner desto steiler.
+    :param LEDs: Eine Liste mit Objekten der Klasse LED
+    :return:
+    """
+    i = 0
+    while i <= 24:
+        if i%fade == 0:
+            for LED in LEDs:
+                if LED.farbe == "rot":
+                    LED.helligkeit = i
+                if LED.farbe == "blau":
+                    LED.helligkeit = i
+                if LED.farbe == "gruen":
+                    LED.helligkeit = i
+                if LED.farbe == "weiß":
+                    LED.helligkeit = i
+                if LED.farbe == "uv":
+                    LED.helligkeit = i
+                if LED.farbe == "ir":
+                    LED.helligkeit = i
+                # LED.update()
+        i+=1
+        print([LED.get(LED) for LED in LEDs])
+
 ########################################################################################################################
                                                     #TEST#
 ########################################################################################################################
-# def tagsim(LEDs):
-#     for LED in LEDs:
-#         if LED.farbe == "rot":
+
 #
-# Namen = ["eins", "zwei", "drei"]
-# Farben = ["rot", "blau", "UV"]
-# Frequenzen = [700, 1200, 1600]
-# Helligkeit = [1000, 2000, 4000]
-# A = init_LED(Namen, Farben, Frequenzen, Helligkeit, 3)
+
+A = init_LED(Namen, Farben,  Helligkeit, 16)
+tagsim(A,2)
 # for i in A:
-#     print(i.get_LED(i))
+#     print(i.get(i))
+
+
+Namen = []
+Fraben = []
+Helligkeiten = []
+for i in range(16):
+    if i in [0,3,7,12,15]:
+        Fraben.append("rot")
+        Helligkeiten.append(0)
+        Namen.append(i)
+    if i in  [1,2,8,14]:
+        Fraben.append("blau")
+        Helligkeiten.append(0)
+        Namen.append(i)
+    if i in [4,10,13]:
+        Fraben.append("weiß")
+        Helligkeiten.append(0)
+        Namen.append(i)
+    if i in [9]:
+        Fraben.append("uv")
+        Helligkeiten.append(0)
+        Namen.append(i)
+    if i in [5]:
+        Fraben.append("ir")
+        Helligkeiten.append(0)
+        Namen.append(i)
+    if i in [11]:
+        Fraben.append("gruen")
+        Helligkeiten.append(0)
+        Namen.append(i)
+print(Fraben)
+print(Namen)
+print(Helligkeiten)
